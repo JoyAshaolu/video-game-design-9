9ff6ff825994c1110f8cde2eb2607212e93bc208
JoyAshaolu <Joya406@stemcollegiate.ca>
2025-02-13 10:42:00 -0700

    Create feb.13.2025.cs

---
+++ feb.13.2025.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+Console.WriteLine("This is your first code!");
+Console.Write("You wrote your first lines of code!");
+//This is my Comment for you people
