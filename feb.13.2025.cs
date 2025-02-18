6cbd488fbff51f8a9e8fdaee1c55bf13325535ec
JoyAshaolu <Joya406@stemcollegiate.ca>
2025-02-18 12:17:28 -0700

    Update feb.18.2025.cs

--- feb.13.2025.cs
+++ feb.13.2025.cs
@@ -3,3 +3,10 @@ Console.WriteLine();
 Console.WriteLine("This is your first code!");
 Console.Write("You wrote your first lines of code!");
 //This is my Comment for you people
+string firstName;
+firstName = "Billie Jean";
+Console.WriteLine(firstName);
+firstName = "Bobby Brown";
+Console.WriteLine(firstName);
+firstName = "Crappy Pants";
+Console.WriteLine(firstName);
