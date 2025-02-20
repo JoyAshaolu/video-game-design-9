b3db3ece64d507e66e00d4a6442f81a64889bf4c
JoyAshaolu <Joya406@stemcollegiate.ca>
2025-02-20 10:32:20 -0700

    Create feb 20 2025.cs

---
+++ feb 20 2025.cs
@@ -0,0 +1,10 @@
+string firstName;
+firstName = "Billie Jean";
+Console.WriteLine(firstName);
+firstName = "Micheal Jackson";
+Console.WriteLine(firstName);
+firstName = "John Pork";
+Console.WriteLine(firstName);
+firstName = "Popeye";
+Console.WriteLine(firstName);
+//I learned how to write Names with variables 😁😁😁
