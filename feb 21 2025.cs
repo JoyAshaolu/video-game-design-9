e48d38aa967722c5f53fd1f3555ceb4bd4ea902f
JoyAshaolu <Joya406@stemcollegiate.ca>
2025-02-21 08:57:13 -0700

    Create feb 21 2025.cs

---
+++ feb 21 2025.cs
@@ -0,0 +1,4 @@
+string firstName = "Billie Jean";
+int number = 100;
+double temp = 109.9;
+Console.WriteLine($" Hello, {firstName}! you have {number} messages in your inbox. The temperature is {temp} celsius.");
